055a0ebb0a5d4371215a1914ceb3d47fb6b95ada
abauruel <abauruel@gmail.com>
2020-06-12 09:54:36 -0300

    added serializer

--- backend/src/modules/appointments/services/ListProviderAppointmentService.ts
+++ backend/src/modules/appointments/services/ListProviderAppointmentService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import ICacheProviders from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import IAppointmentRepository from '../repositories/IAppoinmentRepository';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
@@ -40,7 +41,7 @@ class ListProviderAppointmentService {
       });
     }
 
-    await this.cacheProvider.save(cacheKey, appointments);
+    await this.cacheProvider.save(cacheKey, classToClass(appointments));
     return appointments;
   }
 }
