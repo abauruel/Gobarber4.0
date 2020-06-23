857e3d72b0ee28abc23b7a942152c503a53b31be
abauruel <abauruel@gmail.com>
2020-06-22 21:53:30 -0300

    added new interface

--- appgobarber/src/hooks/Auth.tsx
+++ appgobarber/src/hooks/Auth.tsx
@@ -17,10 +17,18 @@ interface SignInCredentials {
   email: string;
   password: string;
 }
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
+  updateUser(user: User): Promise<void>;
   loading: boolean;
 }
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -44,6 +52,7 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -63,14 +72,27 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+    api.defaults.headers.authorization = `Bearer ${token}`;
     setData({ token, user });
   }, []);
+  const updateUser = useCallback(
+    async (user: User) => {
+      setData({
+        token: data.token,
+        user,
+      });
+      await AsyncStorage.setItem('@GoBarber:user', JSON.stringify(user));
+    },
+    [setData, data.token],
+  );
   const signOut = useCallback(async () => {
     await AsyncStorage.multiRemove(['@GoBarber:token', '@GoBarber:user']);
     setData({} as AuthState);
   }, []);
   return (
-    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user: data.user, loading, signIn, updateUser, signOut }}
+    >
       {children}
     </AuthContext.Provider>
   );
